1e4c81d56bf4ae4092c00a9d14c8897aa2223781
Alejandro Cerezo <alce65@hotmail.es>
2025-06-23 16:38:11 +0200

    Update feature user context with different repo

--- projects/demo2/src/users.context/context/provider.tsx
+++ projects/demo2/src/users.context/context/provider.tsx
@@ -3,19 +3,22 @@ import { UserContext } from './context';
 import type { User } from '../types/user';
 import { InMemoryUserRepo } from '../services/inmemory-user-repo';
 import type { UserRepo } from '../services/user-repo';
+// import { ApiUserRepo } from '../services/api-user-repo';
+// import { BASE_URL } from '../../core/config';
 
 export const UserContextProvider: React.FC<PropsWithChildren> = ({
     children,
 }) => {
+     const repo: UserRepo = new InMemoryUserRepo();
+    // const baseUrl = import.meta.env.VITE_API_BASE_URL;
+    // const repo: UserRepo = new ApiUserRepo(BASE_URL);
 
-    const repo: UserRepo = new InMemoryUserRepo();
     const [users, setUsers] = useState<User[]>([]);
-
 
     const context: UserContext = {
         users: users,
         setUsers: setUsers,
-        repo
+        repo,
     };
 
     // Desde React 19
